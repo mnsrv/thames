b4efa912fdcbba1e0b67ba7c8aec4320e267dee4
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-03-14 10:04:15 +0300

    fix Object.keys

--- scrape.js
+++ scrape.js
@@ -40,7 +40,7 @@ const scrapeLetterboxd = (res, connection, page = 1, movies = []) => {
   }).catch(error => res.send(error))
 }
 const insertIntoTable  = (res, connection, data) => {
-  const values = Object.keys(data).map(key => data[key])
+  const values = data.map(item => Object.keys(item).map(key => item[key]))
   connection.query(INSERT_MOVIES_QUERY, [values], (err, results) => {
     if (err) {
       if (err) throw err
