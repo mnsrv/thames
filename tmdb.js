7f5fdea1b59cf96b54763e110cb643dcd3a5d14f
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-03-31 05:39:42 +0300

    забираю данные по imdb_id

--- tmdb.js
+++ tmdb.js
@@ -50,17 +50,31 @@ const getInfoFromTMDB = (res, connection, movies, index = 0) => {
     api_key: API_KEY,
     language: 'ru',
     region: 'RU',
-    query,
-    year,
-    page: 1,
-    include_adult: true
+    external_source: 'imdb_id'
   }
-  const url = `https://api.themoviedb.org/3/search/movie?${serialize(params)}`
+  const url = `https://api.themoviedb.org/3/find/${movies[index].imdb_id}?${serialize(params)}`
   axios.get(url)
     .then(response => {
-      updateTableRow(res, connection, dbId, response.data.results[0], movies, index)
+      updateTableRow(res, connection, dbId, response.data.movie_results[0], movies, index)
     })
-    .catch(error => res.send(error))
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    })
 }
 
 const tmdb = (res, connection) => {
